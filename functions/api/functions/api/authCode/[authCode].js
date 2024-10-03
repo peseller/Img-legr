a536c17fb8e9aff16f200521aad91f246185f8f3
peseller <multipeseller@gmail.com>
2024-10-03 12:19:11 +0800

    Create [authCode].js

---
+++ functions/api/functions/api/authCode/[authCode].js
@@ -0,0 +1,31 @@
+function UnauthorizedException(reason) {
+  return new Response(reason, {
+    status: 401,
+    statusText: "Unauthorized",
+    headers: {
+      "Content-Type": "text/plain;charset=UTF-8",
+      // Disables caching by default.
+      "Cache-Control": "no-store",
+      // Returns the "Content-Length" header for HTTP HEAD requests.
+      "Content-Length": reason.length,
+    },
+  });
+}
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+  console.log(env);
+  console.log(params.code);
+  if (decodeURIComponent(params.authCode) === env.AUTH_CODE) {
+    return new Response("ok");
+  } else {
+    return new UnauthorizedException("error");
+  }
+}
